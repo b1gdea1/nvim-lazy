c0efa5ffa09786909fb72bbd18971840cb1a5f5a
rat <1041298337@qq.com>
2023-11-05 23:19:01 +0800

    init

---
+++ lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local opt = vim.opt
+
+-- 行号
+opt.number = true
+opt.relativenumber = false
+
+-- 缩进
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
